0873d8b38003e53abcb62abc86cf660855af7ba1
Carlos Henrique <carloshj2011@gmail.com>
2018-06-27 18:06:51 -0300

    Incluindo EF

---
+++ EntityFramework_Study/Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProductDbContext : DbContext
+    {
+        protected ProductDbContext() : base("Name=ProductDb")
+        {
+            // Cria a base de dados (ProductDb) caso não exista. Os parâmetros de configuração da base estão no app.config.
+            Database.SetInitializer<ProductDbContext>(
+                new CreateDatabaseIfNotExists<ProductDbContext>());
+        }
+    }
+}
